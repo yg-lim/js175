e92128fb708c3e419c7ef218555e0fcccf0fe66c
qseudo <57642143+qseudo@users.noreply.github.com>
2022-08-07 15:52:52 -0400

    completes dice rolling app

--- dice_rolling_app/app.js
+++ dice_rolling_app/app.js
@@ -1,38 +1,51 @@
-function rollDie(min, max) {
-  return Math.floor((Math.random() * (max - min + 1)) + min);
+const HTTP = require('http');
+const PORT = 3000;
+const { URL } = require('url');
+const BASE_URL = `http://localhost:${PORT}`;
+
+function dieRoll(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const HTTP = require('http');
-const URL = require('url').URL; // imports URL class
-const PORT = 3000;
+function rollDice(params) {
+  let numberOfRolls = Number(params.get('rolls'));
+  let numberOfSides = Number(params.get('sides'));
+
+  let result = ``;
+  let counter = numberOfRolls;
+  while (counter > 0) {
+    result += `${dieRoll(1, numberOfSides)}\n`;
+    counter -= 1;
+  }
+
+  return result;
+}
+
+function getParams(pathAndQueryString) {
+  let url = new URL(pathAndQueryString, BASE_URL);
+  return url.searchParams;
+}
 
 const SERVER = HTTP.createServer((req, res) => {
   let method = req.method;
-  let path = req.url;
-  let url = new URL(path, 'http://localhost:3000');
-  let params = url.searchParams;
+  let pathAndQueryString = req.url;
 
-  if (path === '/favicon.ico') {
+  if (pathAndQueryString === '/favicon.ico') {
     res.statusCode = 404;
+    res.write(`${method} ${pathAndQueryString}\n`);
+    res.write(`Favicon not found!\n`);
     res.end();
   } else {
-    let rollsCounter = params.get('rolls');
-    let numOfSides = params.get('sides');
+    let params = getParams(pathAndQueryString);
+    let content = rollDice(params);
 
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-
-    while (rollsCounter > 0) {
-      let dieResult = rollDie(1, numOfSides);
-      res.write(`You rolled the dice. It's a ${dieResult}!\n`);
-      rollsCounter -= 1;
-    }
-
-    res.write(`${method} ${path}\n`);
+    res.write(`${method} ${pathAndQueryString}\n`);
+    res.write(`${content}`);
     res.end();
   }
 });
 
 SERVER.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Listening on port number ${PORT}...`);
 });
