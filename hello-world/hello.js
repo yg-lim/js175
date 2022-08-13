182d776126ec9991d7368b1719ab04b23e61fc7a
qseudo <57642143+qseudo@users.noreply.github.com>
2022-08-13 16:05:53 -0400

    completes hello world

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,12 +1,67 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
+const COUNTRY_DATA = [
+  {
+    path: '/english',
+    flag: 'flag-of-United-States-of-America.png',
+    alt: 'US Flag',
+    title: 'Go to US English site',
+  },
+  {
+    path: '/french',
+    flag: 'flag-of-France.png',
+    alt: 'Drapeau de la france',
+    title: 'Aller sur le site français',
+  },
+  {
+    path: '/serbian',
+    flag: 'flag-of-Serbia.png',
+    alt: 'Застава Србије',
+    title: 'Идите на српски сајт',
+  },
+];
+const LANGUAGE_CODES = {
+  english: 'en-US',
+  french: 'fr-FR',
+  serbian: 'sr-Cryl-rs',
+};
 
-app.set("views", "./views");
-app.set("view engine", "pug");
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+app.use(morgan('common'));
+
+app.locals.currentPathClass = (path, currentPath) => {
+  return path === currentPath ? 'current' : '';
+};
+
+app.locals.countryData = COUNTRY_DATA;
 
 app.get('/', (req, res) => {
-  res.render("hello-world-english");
+  res.redirect('/english');
+});
+
+app.get('/:language', (req, res, next) => {
+  const language = req.params.language;
+  const languageCode = LANGUAGE_CODES[language];
+
+  if (!languageCode) {
+    next(new Error(`Language not supported: ${language}`));
+  } else {
+    res.render(`hello-world-${language}`, {
+      countryData: COUNTRY_DATA,
+      currentPath: req.path,
+      language: languageCode,
+    });
+  }
+});
+
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
 });
 
 app.listen(PORT, 'localhost', () => {
